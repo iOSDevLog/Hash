db37649bb40cca4474c0ce5951543e0698558ffe
jiaxianhua <jiaxianhua@gmail.com>
2025-06-05 13:36:07 +0800

    修复应用程序退出问题：关闭窗口时完全退出应用程序

--- Hash/HashApp.swift
+++ Hash/HashApp.swift
@@ -7,11 +7,23 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
+
 @main
 struct HashApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .windowResizability(.contentSize)
+        .commands {
+            CommandGroup(replacing: .newItem) { }
         }
     }
 }
